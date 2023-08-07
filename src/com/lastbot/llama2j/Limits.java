f6dead44f24660ed163d858078c33dfc5fbefd14
Tero Heinonen <tero@lastbot.com>
2023-08-07 14:03:56 -0700

    Add class for storing system wide limits

---
+++ src/com/lastbot/llama2j/Limits.java
@@ -0,0 +1,6 @@
+package com.lastbot.llama2j;
+
+public class Limits {
+//    public static final int FLOAT_ARRAY_MAX_SIZE = 0x7ffffffd;
+    public static final int FLOAT_ARRAY_MAX_SIZE = 32 * (Integer.MAX_VALUE / 32);
+}
