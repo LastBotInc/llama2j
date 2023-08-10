f69801dd8ec22b97371a396c74552530664752ec
Tero Heinonen <tero@lastbot.com>
2023-08-10 15:42:38 -0700

    Add toString()

--- src/com/lastbot/llama2j/Config.java
+++ src/com/lastbot/llama2j/Config.java
@@ -11,4 +11,17 @@ public class Config {
     int n_kv_heads; // number of key/value heads (can be < query heads because of multiquery)
     int vocab_size; // vocabulary size, usually 256 (byte-level)
     int seq_len; // max sequence length
+
+    @Override
+    public String toString() {
+        return "Config{" +
+                "dim=" + dim +
+                ", hidden_dim=" + hidden_dim +
+                ", n_layers=" + n_layers +
+                ", n_heads=" + n_heads +
+                ", n_kv_heads=" + n_kv_heads +
+                ", vocab_size=" + vocab_size +
+                ", seq_len=" + seq_len +
+                '}';
+    }
 }
