cefd30ef0c94b18c72dc6a8926f4e0bc1ee581cf
Tero Heinonen <tero@lastbot.com>
2023-08-15 21:17:03 -0700

    Updates from upstream for l_wk, l_wv, l_wo: calculation based on n_kv_heads, not dim.

--- src/com/lastbot/llama2j/TransformerWeights.java
+++ src/com/lastbot/llama2j/TransformerWeights.java
@@ -5,18 +5,15 @@ import jcuda.Pointer;
 import static com.lastbot.llama2j.LayerMemoryUtil.allocateAndCopyLayers;
 
 public class TransformerWeights {
-    private final Context context;
-    private final Config config;  // token embedding table
-
     float[] token_embedding_table;    // (vocab_size, dim)
     // weights for rmsnorms
     float[] l_rms_att_weight; // (layer, dim) rmsnorm weights
     float[] l_rms_ffn_weight; // (layer, dim)
-    // weights for matmuls
-    float[] l_wq; // (layer, dim, dim)
-    float[] l_wk; // (layer, dim, dim)
-    float[] l_wv; // (layer, dim, dim)
-    float[] l_wo; // (layer, dim, dim)
+    // weights for matmuls. note dim == n_heads * head_size
+    float[] l_wq; // (layer, dim, n_heads * head_size)
+    float[] l_wk; // (layer, dim, n_kv_heads * head_size)
+    float[] l_wv; // (layer, dim, n_kv_heads * head_size)
+    float[] l_wo; // (layer, n_heads * head_size, dim)
     // weights for ffn
     float[] l_w1; // (layer, hidden_dim, dim)
     float[] l_w2; // (layer, dim, hidden_dim)
@@ -69,28 +66,26 @@ public class TransformerWeights {
     Pointer freq_cis_imagCU; // (seq_len, dim/2)
     Pointer wclsCU;
 
-    public TransformerWeights(Context context, BinFileReader reader, Config config, boolean sharedWeights) {
-        this.context = context;
-        this.config = config;
-
+    public TransformerWeights(Context context, BinFileReader reader, Config p, boolean sharedWeights) {
+        // token embedding table
+        int head_size = p.dim / p.n_heads;
         // always read weights first to CPU memory
         long t0 = System.currentTimeMillis();
-        token_embedding_table = reader.nextFloatArray(config.vocab_size * config.dim);
-        l_rms_att_weight = reader.nextFloatArray(config.n_layers * config.dim);
-        l_wq = reader.nextFloatArray(config.n_layers * config.dim * config.dim);
-        l_wk = reader.nextFloatArray(config.n_layers * config.dim * config.dim);
-        l_wv = reader.nextFloatArray(config.n_layers * config.dim * config.dim);
-        l_wo = reader.nextFloatArray(config.n_layers * config.dim * config.dim);
-        l_rms_ffn_weight = reader.nextFloatArray(config.n_layers * config.dim);
-        l_w1 = reader.nextFloatArray(config.n_layers * config.dim * config.hidden_dim);
-        l_w2 = reader.nextFloatArray(config.n_layers * config.hidden_dim * config.dim);
-        l_w3 = reader.nextFloatArray(config.n_layers * config.dim * config.hidden_dim);
-        rms_final_weight = reader.nextFloatArray(config.dim);
-        int head_size = config.dim / config.n_heads;
-        freq_cis_real = reader.nextFloatArray(config.seq_len * head_size / 2);
-        freq_cis_imag = reader.nextFloatArray(config.seq_len * head_size / 2);
+        token_embedding_table = reader.nextFloatArray(p.vocab_size * p.dim);
+        l_rms_att_weight = reader.nextFloatArray(p.n_layers * p.dim);
+        l_wq = reader.nextFloatArray(p.n_layers * p.dim * (p.n_heads * head_size));
+        l_wk = reader.nextFloatArray(p.n_layers * p.dim * (p.n_kv_heads * head_size)); // zzz
+        l_wv = reader.nextFloatArray(p.n_layers * p.dim * (p.n_kv_heads * head_size)); // zzz
+        l_wo = reader.nextFloatArray(p.n_layers * (p.n_heads * head_size) * p.dim);
+        l_rms_ffn_weight = reader.nextFloatArray(p.n_layers * p.dim);
+        l_w1 = reader.nextFloatArray(p.n_layers * p.dim * p.hidden_dim);
+        l_w2 = reader.nextFloatArray(p.n_layers * p.hidden_dim * p.dim);
+        l_w3 = reader.nextFloatArray(p.n_layers * p.dim * p.hidden_dim);
+        rms_final_weight = reader.nextFloatArray(p.dim);
+        freq_cis_real = reader.nextFloatArray(p.seq_len * head_size / 2);
+        freq_cis_imag = reader.nextFloatArray(p.seq_len * head_size / 2);
 
-        wcls = sharedWeights ? token_embedding_table : reader.nextFloatArray(config.vocab_size * config.dim);
+        wcls = sharedWeights ? token_embedding_table : reader.nextFloatArray(p.vocab_size * p.dim);
         long t1 = System.currentTimeMillis();
         LLogger.time("Create TransformerWeights CPU", t0, t1);
 
@@ -103,24 +98,15 @@ public class TransformerWeights {
                 int firstLayer = context.layerAllocation.firstLayer[dev];
                 int lastLayer = context.layerAllocation.lastLayer[dev];
 
-                l_rms_att_weightCU = allocateAndCopyLayers(cu, l_rms_att_weight, firstLayer, lastLayer, config.n_layers);
-                l_wqCU = allocateAndCopyLayers(cu, l_wq, firstLayer, lastLayer, config.n_layers);
-                l_wkCU = allocateAndCopyLayers(cu, l_wk, firstLayer, lastLayer, config.n_layers);
-                l_wvCU = allocateAndCopyLayers(cu, l_wv, firstLayer, lastLayer, config.n_layers);
-                l_woCU = allocateAndCopyLayers(cu, l_wo, firstLayer, lastLayer, config.n_layers);
-                l_rms_ffn_weightCU = allocateAndCopyLayers(cu, l_rms_ffn_weight, firstLayer, lastLayer, config.n_layers);
-                l_w1CU = allocateAndCopyLayers(cu, l_w1, firstLayer, lastLayer, config.n_layers);
-                l_w2CU = allocateAndCopyLayers(cu, l_w2, firstLayer, lastLayer, config.n_layers);
-                l_w3CU = allocateAndCopyLayers(cu, l_w3, firstLayer, lastLayer, config.n_layers);
-//                l_rms_att_weightCU = cu.allocateAndCopyToDevice(l_rms_att_weight);
-//                l_wqCU = cu.allocateAndCopyToDevice(l_wq);
-//                l_wkCU = cu.allocateAndCopyToDevice(l_wk);
-//                l_wvCU = cu.allocateAndCopyToDevice(l_wv);
-//                l_woCU = cu.allocateAndCopyToDevice(l_wo);
-//                l_rms_ffn_weightCU = cu.allocateAndCopyToDevice(l_rms_ffn_weight);
-//                l_w1CU = cu.allocateAndCopyToDevice(l_w1);
-//                l_w2CU = cu.allocateAndCopyToDevice(l_w2);
-//                l_w3CU = cu.allocateAndCopyToDevice(l_w3);
+                l_rms_att_weightCU = allocateAndCopyLayers(cu, l_rms_att_weight, firstLayer, lastLayer, p.n_layers);
+                l_wqCU = allocateAndCopyLayers(cu, l_wq, firstLayer, lastLayer, p.n_layers);
+                l_wkCU = allocateAndCopyLayers(cu, l_wk, firstLayer, lastLayer, p.n_layers);
+                l_wvCU = allocateAndCopyLayers(cu, l_wv, firstLayer, lastLayer, p.n_layers);
+                l_woCU = allocateAndCopyLayers(cu, l_wo, firstLayer, lastLayer, p.n_layers);
+                l_rms_ffn_weightCU = allocateAndCopyLayers(cu, l_rms_ffn_weight, firstLayer, lastLayer, p.n_layers);
+                l_w1CU = allocateAndCopyLayers(cu, l_w1, firstLayer, lastLayer, p.n_layers);
+                l_w2CU = allocateAndCopyLayers(cu, l_w2, firstLayer, lastLayer, p.n_layers);
+                l_w3CU = allocateAndCopyLayers(cu, l_w3, firstLayer, lastLayer, p.n_layers);
                 rms_final_weightCU = cu.allocateAndCopyToDevice(rms_final_weight, true);
                 freq_cis_realCU = cu.allocateAndCopyToDevice(freq_cis_real, true);
                 freq_cis_imagCU = cu.allocateAndCopyToDevice(freq_cis_imag, true);
