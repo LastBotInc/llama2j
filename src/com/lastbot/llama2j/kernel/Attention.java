9f4065a80500d28ac9035a6ea473db3334e6573f
Tero Heinonen <tero@lastbot.com>
2023-08-16 15:28:07 -0700

    Fixed and tested Attention

--- src/com/lastbot/llama2j/kernel/Attention.java
+++ src/com/lastbot/llama2j/kernel/Attention.java
@@ -11,21 +11,24 @@ import java.util.Arrays;
 
 import static jcuda.driver.JCudaDriver.cuLaunchKernel;
 
-public class AttentionScore extends Kernel {
+public class Attention extends Kernel {
     private static final int SMALL_KERNEL = 1024;
 
     private final CUfunction smallKernel;
 
-    public AttentionScore(ContextCUDA cuda) {
-        super(cuda, "attentionScore");
+    public Attention(ContextCUDA cuda) {
+        super(cuda, "attention");
+        cuda.setDevice();
         smallKernel = createSmall();
     }
 
     public static void call(float[] score, float[] q, float[] l_key_cache, int queryIndex, int keyIndex, int head_size) {
         // calculate the attention score as the dot product of q and k
         float sum = 0.0f;
+        float value;
         for (int i = 0; i < head_size; i++) {
-            sum += q[queryIndex + i] * l_key_cache[keyIndex + i];
+            value = q[queryIndex + i] * l_key_cache[keyIndex + i];
+            sum += value;
         }
         score[0] = sum / (float) Math.sqrt(head_size);
     }
@@ -38,6 +41,7 @@ public class AttentionScore extends Kernel {
         cuda.synchronizeTransfer();
 
         call(0, pScore, pq, pL_key_cache, queryIndex, keyIndex, head_size);
+
         cuda.synchronizeKernel(0);
         cuda.copyFromDeviceToHost(pScore, score);
         cuda.synchronizeTransfer();
@@ -47,8 +51,15 @@ public class AttentionScore extends Kernel {
 
         call(copyOfScore, q, l_key_cache, queryIndex, keyIndex, head_size);
 
-        compareWithThreshold("AttentionScore.call score ",
+        compareWithThreshold("Attention.call score ",
                 score, copyOfScore, 1e-5f);
+    }
+
+    private static void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+        }
     }
 
     public void call(int kernelStreamId, Pointer score, Pointer q, Pointer l_key_cache, int queryIndex, int keyIndex,
@@ -56,14 +67,15 @@ public class AttentionScore extends Kernel {
         CUstream stream = cuda.getCUKernelStream(kernelStreamId);
         if (head_size <= SMALL_KERNEL && head_size % 2 == 0) {
             int blockSizeX = findNextPowerOf2(head_size);
+//            int blockSizeX = head_size;
             int gridSizeX = 1;
             int sharedMemory = blockSizeX * Sizeof.FLOAT;
 
 //            __global__ void attentionScore(float[] score, float[] q, float[] l_key_cache, int head_size) {
             Pointer kernelParameters = Pointer.to(
                     Pointer.to(score),
-                    Pointer.to(q).withByteOffset((long) queryIndex * Sizeof.FLOAT),
-                    Pointer.to(l_key_cache).withByteOffset((long) keyIndex * Sizeof.FLOAT),
+                    Pointer.to(q.withByteOffset((long) queryIndex * Sizeof.FLOAT)),
+                    Pointer.to(l_key_cache.withByteOffset((long) keyIndex * Sizeof.FLOAT)),
                     Pointer.to(new int[]{head_size})
             );
 
@@ -82,44 +94,32 @@ public class AttentionScore extends Kernel {
         String code =
                 """
                             extern "C"
-                            __global__ void attentionScore(float* score, float* q, float* l_key_cache, int head_size) {
+                            __global__ void attention(float* score, float* q, float* k, int head_size) {
                                 int tid = threadIdx.x;
 
                                 extern __shared__ float sdata[];
 
                                 if (tid < head_size) {
-                                    // Store the value in shared memory for reduction
-                                    printf(">>> 2 %d\\n", tid);
-                                    sdata[threadIdx.x] = q[tid] * l_key_cache[tid];
+                                    sdata[tid] = q[tid] * k[tid];
                                 } else {
-                                    printf(">>> 3 %d\\n", tid);
-                                    sdata[threadIdx.x] = 0.0f;
+                                    sdata[tid] = 0.0f;
                                 }
                                 __syncthreads();  // Ensure all threads in block have stored their values
 
-                                if (tid == 0) {
-                                    printf(">>> 4");
-                                }
-
                                 // Block-wise reduction
                                 for (unsigned int stride = blockDim.x / 2; stride > 0; stride >>= 1) {
-                                    if (threadIdx.x < stride) {
+                                    // if (tid < stride && threadIdx.x + stride < head_size) {
+                                    if (tid < stride) {
                                         sdata[threadIdx.x] += sdata[threadIdx.x + stride];
                                     }
                                     __syncthreads();  // Ensure all threads in block are in sync after each step
                                 }
 
                                 if (tid == 0) {
-                                    printf(">>> 5");
-                                    score[0] = sdata[0] / (float) sqrtf(head_size);
-                                }
-                                __syncthreads();
-
-                                if (tid == 0) {
-                                    printf(">>> 6");
+                                    score[0] = sdata[0] / sqrtf(head_size);
                                 }
                             }
                         """;
-        return loadFromCode(code, "attentionScore");
+        return loadFromCode(code, "attention");
     }
 }
