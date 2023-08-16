04a3e1cdf03bdbc274cf069e2ce70b75a37cd5c1
Tero Heinonen <tero@lastbot.com>
2023-08-15 20:47:54 -0700

    Sync transfer before free in test. Reflect upstream changes in call() and in kernel (NOTE: kernel not tested).

--- src/com/lastbot/llama2j/kernel/ApplyRope.java
+++ src/com/lastbot/llama2j/kernel/ApplyRope.java
@@ -19,25 +19,32 @@ public class ApplyRope extends Kernel {
     }
 
     public static void call(float[] q, float[] k, float[] freq_cis_real, float[] freq_cis_imag,
-                             int dim, int head_size, int freq_cis_imag_row) {
+                            int dim, int kv_dim, int head_size, int freq_cis_imag_row) {
         // RoPE relative positional encoding: complex-valued rotate q and k by freq_cis in each head
+        // keys
         for (int i = 0; i < dim; i += 2) {
-            float q0 = q[i];
-            float q1 = q[i + 1];
-            float k0 = k[i];
-            float k1 = k[i + 1];
+            float v0 = q[i];
+            float v1 = q[i + 1];
             int freq_cis_imag_index = freq_cis_imag_row + (i % head_size) / 2;
             float fcr = freq_cis_real[freq_cis_imag_index];
             float fci = freq_cis_imag[freq_cis_imag_index];
-            q[i] = q0 * fcr - q1 * fci;
-            q[i + 1] = q0 * fci + q1 * fcr;
-            k[i] = k0 * fcr - k1 * fci;
-            k[i + 1] = k0 * fci + k1 * fcr;
+            q[i] = v0 * fcr - v1 * fci;
+            q[i + 1] = v0 * fci + v1 * fcr;
+        }
+        // values
+        for (int i = 0; i < kv_dim; i += 2) {
+            float v0 = k[i];
+            float v1 = k[i + 1];
+            int freq_cis_imag_index = freq_cis_imag_row + (i % head_size) / 2;
+            float fcr = freq_cis_real[freq_cis_imag_index];
+            float fci = freq_cis_imag[freq_cis_imag_index];
+            k[i] = v0 * fcr - v1 * fci;
+            k[i + 1] = v0 * fci + v1 * fcr;
         }
     }
 
     public void test(float[] q, float[] k, float[] freq_cis_real, float[] freq_cis_imag,
-                     int dim, int head_size, int freq_cis_imag_row) {
+                     int dim, int kv_dim, int head_size, int freq_cis_imag_row) {
         float[] copyOfQ = Arrays.copyOf(q, q.length);
         float[] copyOfK = Arrays.copyOf(k, k.length);
         Pointer pq = cuda.allocateAndCopyToDevice(q, false);
@@ -45,16 +52,17 @@ public class ApplyRope extends Kernel {
         Pointer pFreq_cis_real = cuda.allocateAndCopyToDevice(freq_cis_real, false);
         Pointer pFreq_cis_imag = cuda.allocateAndCopyToDevice(freq_cis_imag, false);
         cuda.synchronizeTransfer();
-        call(0, pq, pk, pFreq_cis_real, pFreq_cis_imag, dim, head_size, freq_cis_imag_row);
+        call(0, pq, pk, pFreq_cis_real, pFreq_cis_imag, dim, kv_dim, head_size, freq_cis_imag_row);
         cuda.synchronizeKernel(0);
         cuda.copyFromDeviceToHost(pk, k);
         cuda.copyFromDeviceToHost(pq, q);
+        cuda.synchronizeTransfer();
         cuda.free(pq);
         cuda.free(pk);
         cuda.free(pFreq_cis_real);
         cuda.free(pFreq_cis_imag);
 
-        call(copyOfQ, copyOfK, freq_cis_real, freq_cis_imag, dim, head_size, freq_cis_imag_row);
+        call(copyOfQ, copyOfK, freq_cis_real, freq_cis_imag, dim, kv_dim, head_size, freq_cis_imag_row);
 
         cuda.synchronizeTransfer();
 
@@ -63,7 +71,7 @@ public class ApplyRope extends Kernel {
     }
 
     public void call(int kernelStreamId, Pointer q, Pointer k, Pointer freq_cis_real, Pointer freq_cis_imag,
-                     int dim, int head_size, int freq_cis_imag_row) {
+                     int dim, int kv_dim, int head_size, int freq_cis_imag_row) {
 //        __global__ void applyRope(    float *q, float *k,
 //                                      const float *freq_cis_real, const float *freq_cis_imag,
 //                                      int dim, int head_size, int freq_cis_imag_row)
@@ -73,13 +81,16 @@ public class ApplyRope extends Kernel {
                 Pointer.to(freq_cis_real),
                 Pointer.to(freq_cis_imag),
                 Pointer.to(new int[]{dim}),
+                Pointer.to(new int[]{kv_dim}),
                 Pointer.to(new int[]{head_size}),
                 Pointer.to(new int[]{freq_cis_imag_row})
         );
 
-        int halfDim = dim / 2;
+        // choose larger dimension
+        int halfDim = Math.max(dim / 2, kv_dim / 2);
 
-        int blockSizeX = Math.min(findNextPowerOf2(halfDim), 1024);
+        int blockSizeX = Math.min(findNextPowerOf2(halfDim), MAX_THREADS_PER_BLOCK);
+
         int gridSizeX = (int) Math.ceil((double) halfDim / blockSizeX);
 
         cuLaunchKernel(kernel,
@@ -93,33 +104,36 @@ public class ApplyRope extends Kernel {
     private CUfunction create() {
         String code =
                 """
-                                extern "C"
-                                __global__ void applyRope(float *q, float *k,
-                                const float *freq_cis_real, const float *freq_cis_imag,
-                                int dim, int head_size, int freq_cis_imag_row)
-                                {
-                                    // process elements in steps of 2
-                                    int i = 2 * (blockIdx.x * blockDim.x + threadIdx.x);
-                                 
-                                    // Ensure we don't go out of bounds
-                                    if (i < dim) {
-                                        float q0 = q[i];
-                                        float q1 = q[i + 1];
-                                        float k0 = k[i];
-                                        float k1 = k[i + 1];
+                            extern "C"
+                            __global__ void applyRope(float *q, float *k,
+                            const float *freq_cis_real, const float *freq_cis_imag,
+                            int dim, int kv_dim, int head_size, int freq_cis_imag_row)
+                            {
+                                // process elements in steps of 2
+                                int i = 2 * (blockIdx.x * blockDim.x + threadIdx.x);
+                             
+                                int freq_cis_imag_index = freq_cis_imag_row + (i % head_size) / 2;
+                                float fcr = freq_cis_real[freq_cis_imag_index];
+                                float fci = freq_cis_imag[freq_cis_imag_index];
 
-                                        int freq_cis_imag_index = freq_cis_imag_row + (i % head_size) / 2;
-                                        float fcr = freq_cis_real[freq_cis_imag_index];
-                                        float fci = freq_cis_imag[freq_cis_imag_index];
-
-                                        q[i] = q0 * fcr - q1 * fci;
-                                        q[i + 1] = q0 * fci + q1 * fcr;
-
-                                        k[i] = k0 * fcr - k1 * fci;
-                                        k[i + 1] = k0 * fci + k1 * fcr;
-                                    }
+                                // Ensure we don't go out of bounds
+                                if (i < dim) {
+                                    float q0 = q[i];
+                                    float q1 = q[i + 1];
+                                    q[i] = q0 * fcr - q1 * fci;
+                                    q[i + 1] = q0 * fci + q1 * fcr;
                                 }
+
+                                // Ensure we don't go out of bounds
+                                if (i < kv_dim) {
+                                    float k0 = k[i];
+                                    float k1 = k[i + 1];
+                                    k[i] = k0 * fcr - k1 * fci;
+                                    k[i + 1] = k0 * fci + k1 * fcr;
+                                }
+                            }
                         """;
+
         return loadFromCode(code, "applyRope");
     }
 }
