66cfbcbdcd833fae40c0c706632cb327984f8b85
Tero Heinonen <tero@lastbot.com>
2023-08-18 11:30:15 -0700

    Moved ProbIndex from RunState to Sampler. Interface now accepts logits[] directly instead of RunState. This is to correct a bug when using CUDA the logits did not get copied for non-deterministic samplers.

--- src/com/lastbot/llama2j/Sampler.java
+++ src/com/lastbot/llama2j/Sampler.java
@@ -7,22 +7,27 @@ import java.util.Random;
 
 public class Sampler {
     private final Random random;
+    private final ProbIndex[] probIndex; // buffer used in top-p sampling, CPU only
 
     private float random_f32() { // random float32 in [0,1)
         return random.nextFloat();
     }
 
-    public Sampler(long rngSeed) {
+    public Sampler(long rngSeed, int vocab_size) {
         this.random = new XoRoShiRo128PlusRandom(rngSeed);
+
+        probIndex = new ProbIndex[vocab_size];
+        for (int i = 0; i < vocab_size; i++) {
+            probIndex[i] = new ProbIndex();
+        }
     }
 
-    public int sample(RunState state, int n) {
-        float[] probabilities = state.logits;
+    public int sample(float[] logits, int n) {
         // sample index from probabilities, they must sum to 1
         float r = random_f32();
         float cdf = 0.0f;
         for (int i = 0; i < n; i++) {
-            cdf += probabilities[i];
+            cdf += logits[i];
             if (r < cdf) {
                 return i;
             }
@@ -30,46 +35,62 @@ public class Sampler {
         return n - 1; // in case of rounding errors
     }
 
-    public int argmax(float[] v, int n) {
-        // return argmax of v in elements 0..n
+    public int argmax(float[] logits, int n) {
+        // return argmax of logits in elements 0..n
         int max_i = 0;
-        float max_p = v[0];
+        float max_p = logits[0];
         for (int i = 1; i < n; i++) {
-            if (v[i] > max_p) {
+            if (logits[i] > max_p) {
                 max_i = i;
-                max_p = v[i];
+                max_p = logits[i];
             }
         }
         return max_i;
     }
 
-    public int sample_topp(RunState state, int n, float topp) {
+
+    // struct used when sorting probabilities during top-p sampling, CPU only
+    public static class ProbIndex implements Comparable<ProbIndex> {
+        public float prob;
+        public int index;
+
+        @Override
+        public int compareTo(ProbIndex o) {
+            if (prob > o.prob) {
+                return -1;
+            } else if (prob < o.prob) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    public int sample_topp(float[] logits, int n, float topp) {
         // top-p sampling (or "nucleus sampling") samples from the smallest set of
         // tokens that exceed probability topp. This way we never sample tokens that
-        // have very low probabilities and are less likely to go "off the rails".
-
-        float[] probabilities = state.logits;
+        // have very low logits and are less likely to go "off the rails".
 
         int n0 = 0;
-        // quicksort indices in descending order of probabilities
+        // quicksort indices in descending order of logits
         // values smaller than (1 - topp) / (n - 1) cannot be part of the result
         // so for efficiency we crop these out as candidates before sorting
         float cutoff = (1.0f - topp) / (n - 1);
 
         for (int i = 0; i < n; i++) {
-            if (probabilities[i] >= cutoff) {
-                state.probIndex[n0].index = i;
-                state.probIndex[n0].prob = probabilities[i];
+            if (logits[i] >= cutoff) {
+                probIndex[n0].index = i;
+                probIndex[n0].prob = logits[i];
                 n0++;
             }
         }
-        Arrays.sort(state.probIndex);
+        Arrays.sort(probIndex);
 
         // truncate the list where cumulative probability exceeds topp
         float cumulative_prob = 0.0f;
         int last_idx = n0 - 1; // in case of rounding errors consider all elements
         for (int i = 0; i < n0; i++) {
-            cumulative_prob += state.probIndex[i].prob;
+            cumulative_prob += probIndex[i].prob;
             if (cumulative_prob > topp) {
                 last_idx = i;
                 break; // we've exceeded topp by including last_idx
@@ -80,11 +101,11 @@ public class Sampler {
         float r = random_f32() * cumulative_prob;
         float cdf = 0.0f;
         for (int i = 0; i <= last_idx; i++) {
-            cdf += state.probIndex[i].prob;
+            cdf += probIndex[i].prob;
             if (r < cdf) {
-                return state.probIndex[i].index;
+                return probIndex[i].index;
             }
         }
-        return state.probIndex[last_idx].index; // in case of rounding errors
+        return probIndex[last_idx].index; // in case of rounding errors
     }
 }
