1376b668cf384e75b9ca4d327d043489545c6eed
Tero Heinonen <tero@lastbot.com>
2023-08-21 15:43:59 -0700

    Add QuantArray, a class to encapsulate quantized data in CPU memory.

---
+++ src/com/lastbot/llama2j/QuantArray.java
@@ -0,0 +1,39 @@
+package com.lastbot.llama2j;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public class QuantArray {
+    private final Quant quant;
+    private final byte[] data;
+    private final long floatOffset;
+
+    public QuantArray(Quant quant, byte[] data, long floatOffset) {
+        this.data = data;
+        this.floatOffset = floatOffset;
+        this.quant = quant;
+    }
+
+    public int byteOffsetByFloatIndex(int floatIndex) {
+        int adjustedFloatIndex = (int) (floatIndex - floatOffset);
+        if (adjustedFloatIndex < 0) {
+            throw new RuntimeException("adjustedFloatIndex < 0");
+        }
+        int byteOffset = quant.byteOffsetByFloatIndex(adjustedFloatIndex);
+        return byteOffset;
+    }
+
+    public Quant getQuant() {
+        return quant;
+    }
+
+    public byte[] getByteArray() {
+        return data;
+    }
+
+    public void write(DataOutputStream dos) throws IOException {
+        dos.writeLong(floatOffset);
+        dos.writeInt(data.length);
+        dos.write(data);
+    }
+}
