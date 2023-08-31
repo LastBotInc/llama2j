da5daad21bff1907b9772810f38b2d6b6f957c07
Tero Heinonen <tero@lastbot.com>
2023-08-31 16:36:57 -0700

    -Xmx32g needed when converting the models

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Xmx16g -XX:+UnlockExperimentalVMOptions -XX:+UseZGC --enable-preview -jar target/llama2j-1.0-SNAPSHOT.jar  "$@"
+java -Xmx32g -XX:+UnlockExperimentalVMOptions -XX:+UseZGC --enable-preview -jar target/llama2j-1.0-SNAPSHOT.jar  "$@"
